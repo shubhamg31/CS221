afbc1b0b8c134aeb21e575e6dad473f17cb1623d
Vinaya Polamreddi <vinaya.polamreddi@gmail.com>
2015-10-18 22:36:27 -0700

    Changed from sparse dict to full matrix

--- processDataSet.py
+++ processDataSet.py
@@ -15,13 +15,26 @@ def process(filename):
 	countCuisine = 0
 	with open(filename, 'rb') as dataset:
 		datareader = csv.reader(dataset)
+		tempX = []
 		for row in datareader:
 			cuisine = row[0]
 			if cuisine not in cuisineMap:
 				cuisineMap[cuisine] = countCuisine
 				countCuisine = countCuisine + 1
 			ingredients = collections.Counter(row[1:])
-			X.append(ingredients)
-			Y.append(cuisineMap.get(cuisine))
 			ingredientSet.update(ingredients)
+			tempX.append((ingredients, cuisineMap.get(cuisine)))
+		for ingredients, cuisine in tempX:
+			featureVector = [0] * len(ingredientSet)
+			for index, ingredient in enumerate(ingredientSet):
+				if ingredient in ingredients:
+					featureVector[index] = 1
+				else:
+					featureVector[index] = 0
+			X.append(featureVector)
+			Y.append(cuisineMap.get(cuisine))
 	return X, Y, ingredientSet, cuisineMap
+
+
+x, y, i, c = process('data.csv')
+print x
